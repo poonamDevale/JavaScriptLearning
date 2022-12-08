15bb3b2d403db612ce3314b7939339b920412a2d
poonamDevale <poonamnphule10@gmail.com>
2022-12-08 23:14:33 +0530

    First Page of JS code
    First Page of JS code

---
+++ 01_variable_assign.js
@@ -0,0 +1,15 @@
+var myFullName;
+myFullName="Poonam Devale ";
+console.log(myFullName); 
+var myFullName="Poonam Madhuker Devale";
+console.log(myFullName);
+var myLove= "I Love only JS";
+console.log(myLove);
+var firstName="Poonam";
+var lastName="Devale";
+var age="32";
+var address="Pune";
+var pinCode="411021";
+var gender="Female";
+var married="Yes";
+console.log(firstName,lastName,age,address,pinCode,gender,married)
