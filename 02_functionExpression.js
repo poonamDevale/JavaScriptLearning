8bfbddce9c5d403183694da6f5195295b596b5cf
poonamDevale <poonamnphule10@gmail.com>
2022-12-12 23:17:28 +0530

    Assignment 12 Dce
    Function Expressions and String

---
+++ 02_functionExpression.js
@@ -0,0 +1,16 @@
+var myName = "Mohit";
+console.log(myName);
+
+// Function Expression
+var showResult = function() {
+    console.log("show.....");
+}
+showResult();
+
+// We can store a function as value in JS
+var substraction = function(num1, num2){
+    var result = num1 - num2;
+    console.log("substraction.......", result);
+    return result;
+}
+substraction(45,25);
