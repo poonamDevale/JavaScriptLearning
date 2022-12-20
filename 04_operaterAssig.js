7e6ad29cf6d3ad04c45cf2ea6d124cc1760626d7
poonamDevale <poonamnphule10@gmail.com>
2022-12-20 16:22:16 +0530

    String Operator
    Calculate the string using Arithmetical Operator

---
+++ 04_operaterAssig.js
@@ -0,0 +1,20 @@
+console.log("======================================1=================================");
+
+var javaLength="JavaScript";
+console.log(`1.1)Length of given String is:${javaLength.length} and square of String is:${javaLength.length*javaLength.length}`);
+
+var googleLength="Google Chrome";
+console.log(`1.2)Length of given String is:${googleLength.length} and square of String is:${googleLength.length*googleLength.length}`);
+
+var devLength="Developer Smart";
+console.log(`1.3)Length of given String is:${devLength.length} and square of String is:${devLength.length*devLength.length}`);
+
+console.log("======================================2=================================");
+
+
+var stringLength= function() {
+    var stringLength="I am Angular Developer";
+    console.log(`Length of given String is:${stringLength.length} and Count of total words in string are:${4
+}, Division=${stringLength.length/ 4},  Multiplication=${stringLength.length* 4}`);
+}
+stringLength();
